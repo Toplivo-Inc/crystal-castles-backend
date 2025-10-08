ca204ba159ca9a23f78a6e5dacd6008895be13e5
Damantr <andreonepis@gmail.com>
2025-10-08 21:32:05 +0100

    feat: Added template for docker connection

--- obj/Debug/net9.0/CrystalCastlesBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrystalCastlesBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrystalCastlesBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78911a20d3911d66b0c00a4b7ebafd0ee09fd84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e5907141b3801ac240094ebe440833ccfe67a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrystalCastlesBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrystalCastlesBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
